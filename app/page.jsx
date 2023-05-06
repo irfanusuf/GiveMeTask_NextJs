19fba23aeb5a298d8fb0b38ea5057288b6db5d48
Irfan Yousuf <irfanusuf33@gmail.com>
2023-05-07 02:40:01 +0530

    v1.0.0

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><h1>Hello ifyami </h1>
+
+      <h2> welcome to your first next js app</h2>
+    </div>
+  )
+}
+
+export default page
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode:true,
+    swcMinify:true,
+    experimental:{
+        appDir:true
+    },
+}
 
 module.exports = nextConfig
