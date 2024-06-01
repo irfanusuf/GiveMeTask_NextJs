f85393a1abd442a30fa20b01ac1905e8ca12bde6
irfan usuf <irfanusuf33@gmail.com>
2024-06-02 00:20:54 +0530

    anotherFIX

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,8 @@
 
-import * as React from 'react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./components/shared/Navbar";
 import "../styles/global.scss";
-
-
 
 export const metadata = {
   title: "Give me Task",
@@ -13,23 +11,25 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
-
-
-
   return (
-
-
-
     <html lang="en">
+      {/* <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2164548049159520"
+          crossorigin="anonymous"
+        ></script>
+      
+      </head> */}
       <body>
+        <Navbar />
+        <SpeedInsights/>
+        <Analytics/>
         
-      <Navbar/>
-   
+
         {children}
-        
-        
-        </body>
+      </body>
     </html>
   );
 }
+
